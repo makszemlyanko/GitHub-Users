9ec2597c9afefca6db8252bdf5bedef27c3c724b
kokosDev <maks.kokos.dev@gmail.com>
2022-11-18 14:48:34 +0300

    inital commit

---
+++ GitHub Users/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub Users
+//
+//  Created by Maks Kokos on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
